61a2e3342c4e6579cd1eb4930d41acd68072b694
Eniya03 <eniyasundaram40@gmail.com>
2024-12-02 15:06:30 +0000

    ES18:Typedefinition of new commands

--- cypress/support/types.d.ts
+++ cypress/support/types.d.ts
@@ -1,14 +1,15 @@
 // cypress/support/types.d.ts
 declare namespace Cypress {
   interface Chainable {
-    /**
-     * Custom command to call the Agify API and get an age prediction.
-     * @param name - The name for which to fetch the age prediction.
-     * @example cy.getAgePrediction('John')
-     */
+    
+     //Custom commands to call the Agify API and get an age prediction.
+    
+  
     requestAgifyurl(name: string): Chainable<Cypress.Response<any>>;
     getAgePrediction(name: string): Chainable<Cypress.Response<any>>;
-    sendBatchRequest(names: string[]): Chainable<Response>;
+    sendBatchRequest(namesString: string): Chainable<Response>;
     sendIncorrectUrl(): Chainable<Response>;
+    mockRateLimitExceeded(): Chainable<void>;
+    mockBatchRequest(): Chainable<void>;
   }
 }
